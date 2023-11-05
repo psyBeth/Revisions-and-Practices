df89f227e0d4eb53ceb5243179545bff6fad61d9
Betul <betulkoru06@gmail.com>
2023-11-05 18:10:01 +0300

    getElementByTagName

--- js-dom/selectors.js
+++ js-dom/selectors.js
@@ -5,6 +5,7 @@ document.title = "Intro for JS DOM 👩🏻‍💻"
 //*===========================================
 //*            GETELEMENTBYID()   (fastest method)
 //*===========================================
+
 const header = document.getElementById("header") //this is called a selector
 console.log(header) //there will be null if sth wrong
 
@@ -28,7 +29,7 @@ console.log(h2.innerHTML) //? parse -> ADD NEW ITEM
 //?changing the content of an element
 h2.textContent = "ADD"
 h2.innerText = "ADD ITEM"
-h2.innerHTML = "ADD NEW ITEM" //! this is not secure!! open to hackers adding their script and accessing stuff like cookies
+h2.innerHTML = "ADD NEW ITEM" //! not secure!! open to hackers adding their script and accessing stuff like cookies
 //? innerHTML takes the code that is written
 h2.innerHTML = "<p style='color:red'>DENEME</p>" //? it will show the p tags if written with innerText 
 
@@ -41,6 +42,34 @@ input.value = "JavaScript" //? takes JS in every start
 //*===========================================
 //*          GETELEMENTSBYTAGNAME()
 //*===========================================
+
+//! HTML Collection is a data structure by DOM
+//! not an array but it's Array-Like
+const lists = document.getElementsByTagName("li")
+console.log(lists) //? HTMLCollection(5) [li.list, li.list, li.list, li.list, li.list] // not an array collection
+
+console.log("SIZE:", lists.length)
+
+//? can be iterated witf FOR LOOP
+for(let li of lists){
+    console.log(li.textContent)
+}
+
+//? convert from HTML Collection to array for array methods
+const listArray = [...lists];
+listArray.forEach((li)=> (li.style.color = "green")) //* permanent
+
+//?Array.from() method converts it into an array
+Array.from(lists).map((li)=>(li.style.backgroundColor = "yellow")) //? forEach is better here
+
+//? EXAMPLE 
+const sections = document.getElementsByTagName("section")
+console.log(sections) //? HTMLCollection(2) [section.add-item, section.item-list]
+
+sections[0].style.backgroundColor = "blue"
+sections[0].style.color = "yellow" //? cannot beat the previous one in p tags (inner html priority)
+
+sections[1].style.color = "blue"
 
 //*===========================================
 //*          GETELEMENTSBYCLASSNAME()

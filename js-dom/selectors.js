9700683dd9e50f1f3fa264b56ad23e59f0fa0fa8
Betul <betulkoru06@gmail.com>
2023-11-05 15:56:31 +0300

    id selector

---
+++ js-dom/selectors.js
@@ -0,0 +1,27 @@
+console.log("***** SELECTORS *****")
+
+document.title = "Intro for JS DOM 👩🏻‍💻"
+
+//*===========================================
+//*            GETELEMENTBYID()
+//*===========================================
+const header = document.getElementById("header") //this is called a selector
+console.log(header) //there will be null if sth wrong
+
+header.style.backgroundColor = "green"
+
+//*===========================================
+//*          GETELEMENTSBYTAGNAME()
+//*===========================================
+
+//*===========================================
+//*          GETELEMENTSBYCLASSNAME()
+//*===========================================
+
+//* ========================================
+//*              QUERYSELECTOR()
+//* ========================================
+
+//* ========================================
+//*              QUERYSELECTORALL()
+//* ========================================

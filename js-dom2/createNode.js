3b11bdd4b4bd335698ea3da3b59fea04ade82042
Betul <betulkoru06@gmail.com>
2023-11-11 22:34:23 +0300

    dom events adding features

--- js-dom2/createNode.js
+++ js-dom2/createNode.js
@@ -16,3 +16,34 @@ addItemSection.appendChild(myPar)
 
 console.log(myPar)
 //! element.before() and element.after() connects an element to a DOM tree just like appendChild()
+addItemSection.before(myPar) //* above the add item header
+
+const reactLi = document.querySelector("ul li:nth-child(4)")
+reactLi.after(myPar) //* under the react (<li>)
+
+//* ------- DECLARE ID, CLASS, ETC ------
+//? 1st method
+myPar.id = "paragraf"
+myPar.className = "red center"
+myPar.name = "par"
+myPar.type = "button"
+
+//? 2nd method (setAttribute)
+myPar.setAttribute("id", "deneme")
+myPar.setAttribute("type", "button")
+myPar.setAttribute("class", "bg-warning border border-danger border-3")
+
+//? 3rd method (classList)
+myPar.classList.add("text-center")
+
+console.log(myPar.classList.contains("bg-warning"))
+
+if (myPar.classList.contains("border-danger")) {
+    myPar.classList.remove("border-danger")
+    myPar.classList.add("border-primary")
+}
+
+//? toggle method adds a class if that class hasn't been added before
+//? toggle method removes the class if that specific class has been added before
+myPar.classList.toggle("text-danger")
+

8f283bd4782e95df63b61405c156c5f97f8760ec
Betul <betulkoru06@gmail.com>
2023-12-28 14:57:37 +0300

    async intro

--- async-notes/1-intro.js
+++ async-notes/1-intro.js
@@ -9,12 +9,26 @@
 //? a promise may have one of 3 states: pending, fulfilled, rejected
 
 //* SYNC
-console.log("***SYNC***");
-console.log("BetulBetul");
-const lateing = (period) => {
-    const begin = new Date().getTime()
-    while (new Date().getTime() < begin + period) {}
-}
-console.time("begin") //? start the timer
-lateing(2000) //? blocking for 2000 ms
-console.timeEnd("begin") //? end the timer and show the time period
+// console.log("***SYNC***");
+// console.log("BetulBetul");
+// const lateing = (period) => {
+//     const begin = new Date().getTime()
+//     while (new Date().getTime() < begin + period) {}
+// }
+// console.time("begin") //? start the timer
+// lateing(2000) //? blocking for 2000 ms
+// console.timeEnd("begin") //? end the timer and show the time period
+
+//*ASYNC --- setTimeout()
+const timeoutId = setTimeout(() => {
+    console.log("Time's up");
+} , 1000) //? triggers after 1000 ms
+
+setTimeout(() => {
+    console.log("timeout2 is up");
+} , 500)
+
+console.log("timeout is set"); //?sync
+console.log("timeout is up");  //?sync
+
+clearTimeout(timeoutId) //? cancels timeout

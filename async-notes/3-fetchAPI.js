86975b21cd6b80df504397cca2fb8c3b2c1ed4a6
Betul <betulkoru06@gmail.com>
2023-12-29 16:37:20 +0300

    fetch api strugglin idk

--- async-notes/3-fetchAPI.js
+++ async-notes/3-fetchAPI.js
@@ -9,3 +9,18 @@
 //? fetch() function requires the source parameter that we wanted to bring
 //? gives a response to this promise request
 
+console.log("FETCH");
+let veri = "helloworld"
+fetch("https://api.github.com/user")
+    .then((res) => {
+        if (!res.ok) {
+            //? WE need to catch the error in fetch API
+            throw new Error(`Error: ${res.status}`) //? throwing an error in
+        }
+        return res.json()
+    })
+    .then((data) => {
+        show(data)
+    })
+    .catch((err) => showError(err))
+

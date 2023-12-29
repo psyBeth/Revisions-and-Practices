3b2a20a232eb108268cb6d8e0d7ccbea2abcf8e0
Betul <betulkoru06@gmail.com>
2023-12-29 17:45:01 +0300

    async await 2

--- async2-notes/async-await.js
+++ async2-notes/async-await.js
@@ -14,3 +14,10 @@
 //* await comes before any type of promise based funcion and stops it 
 //* Once the request is fulfilled and the result values ​​are returned, the code continues to run
 
+const getNews = async () => {
+    const API_KEY = "1a1a999e0d7240a6bd2dead87bcca78err"
+    const BASE_URL = "https://newsapi.org/v2/"
+    const queryString = "top-headlines?country=us&category=sport&"
+
+    const res = await fetch(`https://newsapi.org/v2/top-headlines?country=tr&category=sport&apiKey=1a1a999e0d7240a6bd2dead87bcca78e`)
+}  

6abe8ad94f46296c0a341f115e2ca17e8b65c3a7
Betul <betulkoru06@gmail.com>
2023-12-29 17:41:30 +0300

    intro to async await

---
+++ async2-notes/async-await.js
@@ -0,0 +1,16 @@
+//*=================================================
+//*               ASYNC-AWAIT
+//*=================================================
+
+//? Async-Await ECMAScript 2017
+//? Promise structure with easier syntax 
+
+//* to make a function async --> add async keyword before the actual keyword
+
+//* await keyword makes the process wait for the response
+
+//* turning a sync-like code into async code
+
+//* await comes before any type of promise based funcion and stops it 
+//* Once the request is fulfilled and the result values ​​are returned, the code continues to run
+

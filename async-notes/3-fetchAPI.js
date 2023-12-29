412584db9f5279848b496d7bebad9d7ed9184fc9
Betul <betulkoru06@gmail.com>
2023-12-29 16:30:09 +0300

    intro study to fetch api

---
+++ async-notes/3-fetchAPI.js
@@ -0,0 +1,11 @@
+//*========================================
+//*             3 - FETCH API
+//*========================================
+
+//? used to bring data from outside
+//? processing the network requests and managing the responses
+//? most used async method 
+
+//? fetch() function requires the source parameter that we wanted to bring
+//? gives a response to this promise request
+

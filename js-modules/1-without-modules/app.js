9559b7dc78c0168a65ae3ac65dd6a9a969cd8c88
Betul <betulkoru06@gmail.com>
2024-01-04 20:54:12 +0300

    explanation comments added

--- js-modules/1-without-modules/app.js
+++ js-modules/1-without-modules/app.js
@@ -25,3 +25,11 @@ const products = [
 ]
 
 console.log("Total price:", calculatePrice(products));
+
+//? bigger the project, harder to read/improve the code, less sustainable codes without external js files
+
+//* seperating the js files by the functions
+
+//! add the scripts in the html file considering the priority
+
+//! since global namespace is used, a variable name can be defined once for all files (it will give errors if a variable name used more than one file)

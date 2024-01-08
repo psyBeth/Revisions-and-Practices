42f4bb4e6076401848d0e49339ab931042a3c264
Betul <betulkoru06@gmail.com>
2024-01-08 16:09:29 +0300

    ESModule intro import export

--- js-modules/2-browser-es-modules/app.js
+++ js-modules/2-browser-es-modules/app.js
@@ -4,3 +4,11 @@
 //? JS codes can be parsed into different files in an easy and efficient way, allowing them to be violated when necessary
 //? A variable, array, object, function, class; can be made available to other files with the "export" statement
 //? The available ones can be added to the desired file with the "import" statement
+//* this is out main file
+
+import { addit } from "./calculator";
+// imports are hoisted
+
+console.log(" ***ES-Modules*** ");
+
+console.log("TOTAL:", addit(3, 4) );
---
+++ js-modules/2-browser-es-modules/calculator.js
@@ -0,0 +1,16 @@
+//* calculator.JS
+
+//? named export
+export const name = "--Calculator--"
+
+export const PI = 3.1415
+export const primeNos = [2, 3, 5, 7, 11, 13, 17, 23]
+
+export const addit = (a, b) => a + b 
+const subtrac = (a, b) => a - b 
+const multip = (a, b) => a * b 
+const divis = (a, b) => a / b 
+
+const message = () => {
+    console.log("Module finished");
+}

93a3110419e7ec5910cd39b043f52efbd29f58b3
Betul <betulkoru06@gmail.com>
2024-01-08 15:40:49 +0300

    explanation comments on ES Modules

--- js-modules/2-browser-es-modules/app.js
+++ js-modules/2-browser-es-modules/app.js
@@ -1,1 +1,6 @@
 //* ES-MODULES (APP.JS)
+//? ES Modules came with EcmaScript 2015 version
+//? Most of the browsers supports ES Model structure
+//? JS codes can be parsed into different files in an easy and efficient way, allowing them to be violated when necessary
+//? A variable, array, object, function, class; can be made available to other files with the "export" statement
+//? The available ones can be added to the desired file with the "import" statement

003574326dcff450f292ce71c54cdd6625adf0a2
Betul <betulkoru06@gmail.com>
2024-01-10 19:35:41 +0300

    nodejs modules

---
+++ js-modules/3-nodejs-commonjs/app.js
@@ -0,0 +1,5 @@
+const addit = (a, b) => a + b
+exports.addit = addit
+
+const MAX_VALUE = 1000
+exports.maxValue= MAX_VALUE
---
+++ js-modules/3-nodejs-commonjs/index.js
@@ -0,0 +1,5 @@
+console.log("COMMON-JS");
+
+const {addit, maxValue } = require("./app")
+console.log(addit(3,5));
+console.log(maxValue);

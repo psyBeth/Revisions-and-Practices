236f2b52210d44962583651dda8d1fda8b0f930c
Betul <betulkoru06@gmail.com>
2024-01-10 19:37:32 +0300

    nodejs modules explanative comments

--- js-modules/3-nodejs-commonjs/index.js
+++ js-modules/3-nodejs-commonjs/index.js
@@ -3,3 +3,8 @@ console.log("COMMON-JS");
 const {addit, maxValue } = require("./app")
 console.log(addit(3,5));
 console.log(maxValue);
+
+//? It uses common.js for node.js module structures.
+//* exports or module.exports are used to share outside in commonjs.
+//? a shared variable, function, object, etc. To use it, require() method is used.
+//? Nowadays, CommonJS module structure is generally used in backend applications.

c7b1018d705f6aa3493b9e55fc2f9292038d5bc3
Betul <betulkoru06@gmail.com>
2024-01-20 17:30:02 +0300

    OOP intro basic literal

---
+++ js-oop1/1-basic-literal.js
@@ -0,0 +1,11 @@
+const s1 = "Hello" //primitive
+console.log(s1.toUpperCase()); 
+console.log(typeof s1); // string
+
+const s2 = new String("Hello")
+console.log(typeof s2);  //object
+
+// console.log(window);
+// window.alert(1)
+
+console.log(navigator.appVersion);

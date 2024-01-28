fcef9d4359c14e0fcfe0b5234438658d9b55ae8f
Betul <betulkoru06@gmail.com>
2024-01-28 22:33:05 +0300

    Footer.jsx and Main.jsx components added

--- react02-comp-props-style/comp-jsx/src/App.js
+++ react02-comp-props-style/comp-jsx/src/App.js
@@ -1,10 +1,14 @@
+import { Footer } from "./Footer"
 import Header from "./Header"
+import { Main } from "./Main"
 
 function App() {
   return (
     <>
       <h1>React App</h1>
       <Header />
+      <Main />
+      <Footer />
     </>
   )
 }
---
+++ react02-comp-props-style/comp-jsx/src/Main.jsx
@@ -0,0 +1,7 @@
+export const Main = () => {
+  return (
+    <main>
+        <h1>MAIN SECTION</h1>
+    </main>
+  )
+}

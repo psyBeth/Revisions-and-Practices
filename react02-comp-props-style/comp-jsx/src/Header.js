caeb9564379628195e86ff4a19da233a81adcbd9
Betul <betulkoru06@gmail.com>
2024-01-28 23:42:40 +0300

    Header.jsx edited

--- react02-comp-props-style/comp-jsx/src/Header.js
+++ react02-comp-props-style/comp-jsx/src/Header.js
@@ -1,5 +1,14 @@
 function Header() {
-    return <h1>Header</h1>
+   //*   JS alani
+   const mesaj = "hello"
+   console.log(mesaj)
+ 
+   return (
+     <header className="header-main">
+       {/* JSX */}
+       <h1 style={{ color: "violet" }}>Header {mesaj}</h1>
+     </header>
+   )
 }
 
 export default Header

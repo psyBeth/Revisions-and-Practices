72dc7f59ce8228e01801efd18692c169e15339a7
Betul <betulkoru06@gmail.com>
2024-01-30 18:16:36 +0300

    there is a major error i cannot figure out

---
+++ react02-comp-props-style/props-jsx/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import Photo from "./Photo"
+
+const Card = () => {
+  return (
+    <div>
+        <h2>Card</h2>
+        <Photo />
+    </div>
+  )
+}
+
+export default Card
---
+++ react02-comp-props-style/props-jsx/src/components/Photo.jsx
@@ -0,0 +1,7 @@
+const Photo = () => {
+  return (
+    <img src="https://cdn.pixabay.com/photo/2015/06/22/08/40/child-817373_640.jpg" alt="kid" />
+  )
+}
+
+export default Photo

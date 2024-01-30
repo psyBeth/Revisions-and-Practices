0341c7acce8d6d1c0d5e2d5d5da2833b0e838d5d
Betul <betulkoru06@gmail.com>
2024-01-30 18:22:23 +0300

    couldn't figure out

--- react02-comp-props-style/props-jsx/src/components/Photo.jsx
+++ react02-comp-props-style/props-jsx/src/components/Photo.jsx
@@ -1,6 +1,8 @@
 const Photo = () => {
   return (
-    <img src="https://cdn.pixabay.com/photo/2015/06/22/08/40/child-817373_640.jpg" alt="kid" />
+    <div>
+        <img src="https://cdn.pixabay.com/photo/2015/06/22/08/40/child-817373_640.jpg" alt="kid" />
+    </div>
   )
 }
 

1e20bfa8bbb8001893d89370f3487d35a72e0c38
Betul <betulkoru06@gmail.com>
2024-01-31 15:18:20 +0300

    cards made, will now iterate

--- react03-module-css/src/components/card/Card.jsx
+++ react03-module-css/src/components/card/Card.jsx
@@ -6,6 +6,8 @@ const Card = () => {
     console.log(data);
   return (
     <div>
+      
+        //? will be iterating now 
         <h1>{data[0].language}</h1>
         <img src={data[0].img} alt="" />
         <Button name={data[0].btnName} />
@@ -14,7 +16,11 @@ const Card = () => {
         <img src={data[1].img} alt="" />
         <Button name={data[1].btnName} />
 
-        //! iterating will be much more better 
+        <h1>{data[2].language}</h1>
+        <img src={data[2].img} alt="" />
+        <Button name={data[2].btnName} />
+
+         
     </div>
   )
 }

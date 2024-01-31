7426744cd3551031a84c48746fcea70e22bdc52e
Betul <betulkoru06@gmail.com>
2024-01-31 22:27:44 +0300

    card structure with map iteration

--- react04-sass/src/components/Card.jsx
+++ react04-sass/src/components/Card.jsx
@@ -1,17 +1,20 @@
 import "../scss/card.scss"
 
 const Card = ({ veri }) => {
-  const {id, name, job, comment, img} = veri [0]
-  //! ITERATION NEEDED
   return (
     <div>
-      <h2>{name}</h2>
-      <h3>{job}</h3>
-      <img src={img} alt="" />
-      <div>
-        <button>Small</button>
-        <button>Large</button>
-      </div>
+      {veri.map(({ id, name, job, comment, img }) => (
+        <div key={id}>
+          <h2>{name}</h2>
+          <h3>{job}</h3>
+          <img src={img} alt="img" width={"100px"} />
+          <p>{comment}</p>
+          <div>
+            <button>Small</button>
+            <button>Large</button>
+          </div>
+        </div>
+      ))}
     </div>
   )
 }

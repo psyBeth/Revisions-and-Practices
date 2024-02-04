ea67ecfb8e0232157530ec46b9a52f4f0d938020
Betul <betulkoru06@gmail.com>
2024-02-04 21:43:03 +0300

    events 3

--- react05/src/1-events/Events.jsx
+++ react05/src/1-events/Events.jsx
@@ -7,19 +7,32 @@ const Events = () => {
         console.log("Name:", e.target.name);
         console.log("ID:", e.target.id);
     }
+
     const handleDelete = function (){
         console.log("deleted.");
         console.log(message);
     }
-    const message = "BETULKO"
+
+    let message = "BETULKO"   //? static variable
+
+    function handleChange() {
+        message = "Betulko learns React."
+        console.log(message);
+    }
+
+    
   return (
     <div>
         <h1>Events</h1>
+        <h2>{message}</h2>
         <button onClick={handleClick} id="btn" name="my-button">
             Click
         </button>
         <button onClick={() => handleDelete(message)}>
             Delete
+        </button>
+        <button onClick={handleChange}>
+            Change
         </button>
     </div>
   )

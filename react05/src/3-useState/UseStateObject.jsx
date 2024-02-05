0fc20a7ec11a1fd72f72814fd3c8d76b2c80fc5c
Betul <betulkoru06@gmail.com>
2024-02-05 16:45:02 +0300

    all updated

--- react05/src/3-useState/UseStateObject.jsx
+++ react05/src/3-useState/UseStateObject.jsx
@@ -2,20 +2,33 @@ import { useState } from "react"
 
 const UseStateObject = () => {
     
-    const [name, setName] = useState("Baho")
-    const [job, setJob] = useState("developer")
-    const [age, setAge] = useState(21)
+    // const [name, setName] = useState("Baho")
+    // const [job, setJob] = useState("developer")
+    // const [age, setAge] = useState(21)
+
+    const [person, setPerson] = useState({
+        name: "Baho",
+        job: "developer",
+        age: 21,
+    })
+
+    const { name, job, age } = person    //? state dest
+    console.log(person);
 
   return (
     <div>
         <h1>Employee Form</h1>
-        <h2>Name: {name}</h2>
-        <h3>Occupation: {job}</h3>
-        <h3>Age: {age}</h3>
+        <h2>Name: {person.name}</h2>
+        <h3>Occupation: {person.job}</h3>
+        <h3>Age: {person.age}</h3>
 
-        <button onClick={() => setName("Betul")}>update name</button>
-        <button onClick={() => setJob("tester")}>update occupation</button>
-        <button onClick={() => setAge(age + 1)}>update age</button>
+        <button onClick={() => setPerson({...person, name :"Betul", job: "tester", age: "22" })}>
+            update person
+        </button>
+        {/* might also use different buttons for every key-value, like the previous method*/}
+
+        {/* <button onClick={() => setJob("tester")}>update occupation</button>
+        <button onClick={() => setAge(age + 1)}>update age</button> */}
     </div>
   )
 }

e805d3768bd0671e342e87a13b64f7e453fd0250
Betul <betulkoru06@gmail.com>
2024-02-05 21:03:19 +0300

    react bootstrap added

--- react06-forms-otherevents/src/App.js
+++ react06-forms-otherevents/src/App.js
@@ -6,8 +6,8 @@ function App() {
   return ( 
     <div>
       <Forms />
-      <KeyboardClipboard />
-      <MouseEvent />
+      {/* <KeyboardClipboard />
+      <MouseEvent /> */}
     </div>
   );
 }

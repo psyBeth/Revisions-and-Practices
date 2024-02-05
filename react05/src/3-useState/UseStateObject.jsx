0918526c6953c1eb2cd873ad6d9ddad025a73594
Betul <betulkoru06@gmail.com>
2024-02-05 16:53:30 +0300

    useState object

--- react05/src/3-useState/UseStateObject.jsx
+++ react05/src/3-useState/UseStateObject.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import UseStateCounter from "./UseStateCounter"
 
 const UseStateObject = () => {
     
@@ -10,9 +11,35 @@ const UseStateObject = () => {
         name: "Baho",
         job: "developer",
         age: 21,
+        salary: 5000,
     })
 
-    const { name, job, age } = person    //? state dest
+    //   let toggle = false
+    //? Boolean state
+    const [toggle, setToggle] = useState(false)
+
+    const handleSwap = () => {
+        // toggle = !toggle
+        setToggle(!toggle)
+        console.log(toggle)
+        if (toggle) {
+            setPerson({
+            name: "Canan",
+            job: "Developer",
+            age: 22,
+            salary: 200000,
+            })
+        } else {
+            setPerson({
+            name: "Ali",
+            job: "Tester",
+            age: 42,
+            salary: 100000,
+            })
+        }
+    }
+
+    // const { name, job, age } = person    //? state dest
     console.log(person);
 
   return (
@@ -21,6 +48,7 @@ const UseStateObject = () => {
         <h2>Name: {person.name}</h2>
         <h3>Occupation: {person.job}</h3>
         <h3>Age: {person.age}</h3>
+        <h3>Salary: {person.salary}</h3>
 
         <button onClick={() => setPerson({...person, name :"Betul", job: "tester", age: "22" })}>
             update person
@@ -29,6 +57,9 @@ const UseStateObject = () => {
 
         {/* <button onClick={() => setJob("tester")}>update occupation</button>
         <button onClick={() => setAge(age + 1)}>update age</button> */}
+
+        <button onClick={handleSwap}>Swap</button>
+        {toggle && <UseStateCounter />}
     </div>
   )
 }

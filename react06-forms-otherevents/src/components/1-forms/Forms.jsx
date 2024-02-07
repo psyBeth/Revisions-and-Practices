927891b3f2fe59a6b873de9d4289f2bb25a70c0e
Betul <betulkoru06@gmail.com>
2024-02-07 20:58:43 +0300

    state email password

--- react06-forms-otherevents/src/components/1-forms/Forms.jsx
+++ react06-forms-otherevents/src/components/1-forms/Forms.jsx
@@ -18,21 +18,23 @@ const Forms = () => {
     <Container className="mt-4">
     <Form>
       <h1 className='text-danger'>FORMS</h1>
-      <Form.Group className="mb-3" controlId="username">
+      <Form.Group className="mb-3">
         <Form.Label>
           {username && <span className='fw-bold'>Hello {username}</span>}
         </Form.Label>
-        <Form.Control type="text" placeholder="Username" onChange={handleUsername} value={username}/>
+        <Form.Control type="text" placeholder="Username" onChange={handleUsername} value={username} id="username" required/>
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="email">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+      <Form.Group className="mb-3">
+        <Form.Label>
+          Email: <span className='fw-bold'>{email}</span>
+        </Form.Label>
+        <Form.Control type="email" placeholder="Enter email" onChange={(e) => setEmail(e.target.value)} value={email} id="email" required/>
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="password">
+      <Form.Group className="mb-3">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} value={password} id='password' required/>
       </Form.Group>
       
       <Button variant="danger" type="submit">

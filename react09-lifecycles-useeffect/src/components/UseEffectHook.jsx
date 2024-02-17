753be9216cb42ba9b2c5b9dbce2bf357c224dbd4
Betul <betulkoru06@gmail.com>
2024-02-17 23:02:31 +0300

    useEffectHook completed

--- react09-lifecycles-useeffect/src/components/UseEffectHook.jsx
+++ react09-lifecycles-useeffect/src/components/UseEffectHook.jsx
@@ -7,10 +7,67 @@
 //! componentDidMount, componentDidUpdate, and componentWillUnmount
 //! It can be thought of as a combination of methods.
 
+//! useEffect(() => {
+//*   /* ComponentDidMount code */
+//*      Kodlar
+//! }, []);
+
+//! useEffect(() => {
+//*   */ ComponentDidMount + componentDidUpdate code */
+//! }, [state1, state2]);
+
+//! useEffect(() => {
+//?   /* ComponentDidMount code */
+//!   return () => {
+//*     //* componentWillUnmount code */
+//!   };
+//! }, []);
+
+//! useEffect(() => {
+//*   //* componentDidMount code + componentDidUpdate code */
+
+//!   return () => {
+//*     //* componentWillUnmount code */
+//!   };
+//! }, [state1, state2]); //? Dependency Array
+
+import { useEffect, useState } from "react"
 
 const UseEffectHook = () => {
+  const [count, setCount] = useState(0)
+
+  // useEffect(() => {
+  //   //? ComponentDidMount + componentDidUpdate
+  //   console.log("Fetch started")
+
+  //   return () => {
+  //     //? DOM'Dan kaldirildiginda calisti
+  //     console.log("Unmounting")
+  //   }
+  // }, [count]) //? count herdegistiginde
+
+  useEffect(() => {
+    //? ComponentDidMount
+    console.log("Fetch started")
+
+    return () => {
+      //? DOM'Dan kaldirildiginda calisti
+      console.log("Unmounting")
+    }
+  }, []) //? array bos
+
+  console.log("render")
   return (
-    <div>UseEffectHook</div>
+    <div className="container text-center">
+      <h1 className="text-danger">USEEFFECT</h1>
+      <h3>COUNT={count}</h3>
+      <button className="btn btn-info" onClick={() => setCount(count + 1)}>
+        INC
+      </button>
+      <button className="btn btn-danger" onClick={() => setCount(count - 1)}>
+        DEC
+      </button>
+    </div>
   )
 }
 

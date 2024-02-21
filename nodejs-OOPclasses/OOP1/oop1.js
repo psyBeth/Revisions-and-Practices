af0da01eadaeb41a9301a65d07fb8f82e13fb48c
Betul <betulkoru06@gmail.com>
2024-02-21 21:40:38 +0300

    node JS oop1 r5

--- nodejs-OOPclasses/OOP1/oop1.js
+++ nodejs-OOPclasses/OOP1/oop1.js
@@ -20,31 +20,31 @@
 
 /* ---------------------------------------- */
 
-// const Car = {
-//     brand: 'Ford',
-//     model: 'Mustang',
-//     year: '1967',
-//     isAutoGear: true,
-//     colors: ['white', 'red'],
-//     details: {
-//         color1: 'white',
-//         color2: 'red',
-//         engineSize: 4900
-//     },
-//     startEngine: function(param1){
-//         console.log(param1)
-//         return 'Engine runned'
-//     },
-//     getDetails: function(){
-//         // return this
-//         // return this.brand + ' ' + this.model + ' ' + this.year
-//         return this.startEngine('hello')
-//     },
-//     arrowMethod: () => {
-//         //* arrow function is globalScope (not working this keyword in here)
-//         return this // output is {}
-//     }
-// }
+const Car = {
+    brand: 'Ford',
+    model: 'Mustang',
+    year: '1967',
+    isAutoGear: true,
+    colors: ['white', 'red'],
+    details: {
+        color1: 'white',
+        color2: 'red',
+        engineSize: 4900
+    },
+    startEngine: function(param1){
+        console.log(param1)
+        return 'Engine runned'
+    },
+    // getDetails: function(){
+    //     // return this
+    //     // return this.brand + ' ' + this.model + ' ' + this.year
+    //     return this.startEngine('hello')
+    // },
+    // arrowMethod: () => {
+    //     //* arrow function is globalScope (not working this keyword in here)
+    //     return this // output is {}
+    // }
+}
 // console.log(Car.brand);
 // console.log(Car.colors[0]);
 // console.log(Car.startEngine('value'));
@@ -70,9 +70,16 @@ const testArray = ['value0', 'value1', 'value2', 'value3', 'value4']
 // console.log(var0, var1, var2, var9);
 
 //? writing in the exact order is needed
-// const [var0, var1, var2, ...var9] = testArray
-// console.log(var0, var1, var2, var9);
+const [var0, var1, var2, ...var9] = testArray
+console.log(var0, var1, var2, var9);
 
 //? Rest Operator (Toplayıcı) (Eşittirin sol tarafında) (En sonda olmak zorunda.)
 const [firstItem, secondItem, ...others] = testArray
 console.log(firstItem, secondItem, others)
+
+//? Spread Operator (Dağıtıcı) (Eşittirin sağ tarafında)
+const newArray = ['value5', ...testArray, 'value6']
+// const newArray = ['value0', 'value1', 'value2', 'value3', 'value4', 'value5', 'value6']
+console.log(newArray)
+
+/* ------------------------------------------------------- */

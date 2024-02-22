55e150284bfde6ce83da0210662f4adfdfc3eb0d
Betul <betulkoru06@gmail.com>
2024-02-22 21:03:01 +0300

    node JS oop2 r3

--- nodejs-OOPclasses/OOP2/oop2.js
+++ nodejs-OOPclasses/OOP2/oop2.js
@@ -35,11 +35,50 @@
 
 /* ------------------------------------------------------- */
 
-class Car {
+// class Car {
+
+//     isRunning = false
+
+//     constructor(brand, model,year) {
+//         this.brand = brand,
+//         this.model = model,
+//         this.year = year
+//     }
+
+//     runEngine() {
+//         this.isRunning = true
+//         console.log('engine is running...');
+//         return this.isRunning;
+//     }
+// };
+// const Ford = new Car ('Ford', 'Mustang', 1967)
+// console.log(Ford);
+
+// // Ford.isRunning = true // changed it (can also do with a method like runEngine)
+// Ford.runEngine();
+// console.log(Ford);
+
+// const Mercedes = new Car('Mercedes', 'CLK200', 2010);
+// console.log(Mercedes);
+// const Renault = new Car('Renault', 'Megane', 2010)
+// console.log(Renault);
+
+//? INHERITANCE (parent-child relationship)
+
+class Vehicle {
+    vehicleIsActive = false
+    constructor (vehicleType) {
+        this.vehicleType = vehicleType
+    }
+}
+
+
+class Car extends Vehicle {
 
     isRunning = false
 
-    constructor(brand, model,year) {
+    constructor(brand, model,year,vehicleType) {
+        super(vehicleType)
         this.brand = brand,
         this.model = model,
         this.year = year
@@ -51,14 +90,5 @@ class Car {
         return this.isRunning;
     }
 };
-const Ford = new Car ('Ford', 'Mustang', 1967)
-console.log(Ford);
 
-// Ford.isRunning = true // changed it (can also do with a method like runEngine)
-Ford.runEngine();
-console.log(Ford);
-
-const Mercedes = new Car('Mercedes', 'CLK200', 2010);
-console.log(Mercedes);
-const Renault = new Car('Renault', 'Megane', 2010)
-console.log(Renault);
+const Ford = new Car('Ford', 'Mustang', 1967)

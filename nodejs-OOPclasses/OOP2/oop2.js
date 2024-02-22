2af023ae63c1a3a09183d2204e5d7bc767c983cd
Betul <betulkoru06@gmail.com>
2024-02-22 20:32:18 +0300

    notes as comments oop2

--- nodejs-OOPclasses/OOP2/oop2.js
+++ nodejs-OOPclasses/OOP2/oop2.js
@@ -1,31 +1,36 @@
 "use strict"
 
 //* OOP & CLASSES
-
+//? OOP: Object Oriented Programming
+//? DRY: Don't Repeat Yourself
+//? BLUEPRINT: Taslak (Mimarların kullandığı mavi şablon kağıdı)
+//? CLASS: Obje türetmek için kullanılacak şablon.
 
 
 //* Class Decleration:
 // class PascalCaseName{...}
 
 //* Class Expression:
-const PascalCaseClassName = class {
-    undefinedProperty // only definition. (undefinedProperty == undefined)
-    propertyName = 'value' // attribute, field 
+// const PascalCaseClassName = class {
+//     undefinedProperty // only definition. (undefinedProperty == undefined)
+//     propertyName = 'value' // attribute, field 
 
-    constructor (parameter1, parameter2 = 'defaul-value') {
-        this.parameter1 = parameter1
-        this.parameter2 = parameter2
-    }
-    methodName1(){
-        return this
-    }
-}
+//     constructor (parameter1, parameter2 = 'defaul-value') {
+//         this.parameter1 = parameter1
+//         this.parameter2 = parameter2
+//     }
+//     methodName1(){
+//         return this
+//     }
+// }
 
 //? INSTANCE = object derived from a class
 
-const PascalCaseInstanceName = new PascalCaseClassName(0, 1)
-console.log(PascalCaseInstanceName);
-console.log(PascalCaseInstanceName.propertyName);
-console.log(PascalCaseInstanceName.parameter1);
-console.log(PascalCaseInstanceName.methodName1());
-console.log(typeof PascalCaseClassName);
+// const PascalCaseInstanceName = new PascalCaseClassName(0, 1)
+// console.log(PascalCaseInstanceName);
+// console.log(PascalCaseInstanceName.propertyName);
+// console.log(PascalCaseInstanceName.parameter1);
+// console.log(PascalCaseInstanceName.methodName1());
+// console.log(typeof PascalCaseClassName);
+
+/* ------------------------------------------------------- */

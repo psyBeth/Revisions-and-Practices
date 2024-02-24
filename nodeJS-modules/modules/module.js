ec30f93fea101c0f24c186a7fd380b4e0122ea16
Betul <betulkoru06@gmail.com>
2024-02-24 12:38:08 +0300

    alt method single func export

--- nodeJS-modules/modules/module.js
+++ nodeJS-modules/modules/module.js
@@ -2,7 +2,11 @@
 console.log("this line from module");
 
 //? single function
-const testFunction = function(){
+// const testFunction = function(){
+//     console.log("this is a function");
+// }
+// module.exports = testFunction
+
+module.exports = function(){
     console.log("this is a function");
-}
-module.exports = testFunction
+}  // this only works with one single function

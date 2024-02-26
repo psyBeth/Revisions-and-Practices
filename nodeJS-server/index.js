d440f5a752a1628bbdce32bbf2b5eca6d29f653d
Betul <betulkoru06@gmail.com>
2024-02-26 21:58:11 +0300

    res.write() and added mathods + JSON

--- nodeJS-server/index.js
+++ nodeJS-server/index.js
@@ -28,14 +28,59 @@ const app = http.createServer((req, res) => {
     // console.log("******************");
     // console.log(req.url);
     //? end points home: /,  list:,  test:  /test
+    // if(req.url == '/') {
+    // res.end("<h1>wellcome to homepage</h1>")
+    // }
+    // else if(req.url == '/list') {
+    //     res.end('<h2>wellcome to list page</h2>')
+    // }
+    // else if (req.url == '/test'){
+    //     res.end('<h1>wellcome to test page</h1>')
+    // }
+    //* RES.WRITE()
+    // if(req.url == '/') {
+    //     res.write('<h1>this </h1>')
+    //     res.write('<h1>is </h1>')
+    //     res.write('<h1>HOME </h1>')
+    //     res.write('<h1>page </h1>')
+
+    //     res.end()
+    //     }
+    //     else if(req.url == '/list') {
+    //         res.end('<h2>wellcome to list page</h2>')
+    //     }
+    //     else if (req.url == '/test'){
+    //         res.end('<h1>wellcome to test page</h1>')
+    //     }
+    //* ADD METHODS
     if(req.url == '/') {
-    res.end("<h1>wellcome to homepage</h1>")
-    }
-    else if(req.url == '/list') {
-        res.end('<h2>wellcome to list page</h2>')
-    }
-    else if (req.url == '/test'){
-        res.end('<h1>wellcome to test page</h1>')
-    }
+        if(req.methods =='GET') {
+            res.write('<h1>this </h1>')
+            res.write('<h1>is </h1>')
+            res.write('<h1>HOME </h1>')
+            res.write('<h1>page </h1>')
+            res.end()
+        } else if(req.method == 'POST'){
+            res.statusCode = 400
+            res.statusMessage = "post yapilamaz"
+            res.end('can not use this method')
+        }}
+        else if(req.method =='DELETE') {
+            res.writeHead(405, "silme yapilamaz", {
+                "Content-Type" : "text/html",
+                "another-header" : "another content"
+            })
+
+            res.end('<h2>wellcome to list page</h2>')
+
+        }
+        else if (req.url == '/list'){
+            const obj = {
+                "error": false,
+                "message": "this is the list page",
+                "deneme" : "deneme"
+            }
+            res.end(JSON.stringify(obj))
+        }
 });
 app.listen(8000, () => console.log(`server successfully runs : http://${HOST}:${PORT}`));

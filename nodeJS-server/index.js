8b93a33e4b3f8dbd82e4eb396b33eead1f1b278b
Betul <betulkoru06@gmail.com>
2024-02-26 20:57:36 +0300

    const (previous server code) thunder req added

--- nodeJS-server/index.js
+++ nodeJS-server/index.js
@@ -8,7 +8,6 @@ require('dotenv').config();
 
 // const PORT = process.env.PORT || 8000
 // const HOST = process.env.HOST || "127.0.0.1"
-
 const PORT = process.env?.PORT || 8000
 const HOST = process.env?.HOST || "127.0.0.1"
 
@@ -16,8 +15,14 @@ const http = require('node:http')
 
 // http.createServer((param1, param2) => {...})
 // http.createServer((request, response) => {...})
-http.createServer((req, res) => {
+// http.createServer((req, res) => {
 
+//     res.end("<h1>wellcome to my first node.js server</h1>")
+
+// }).listen(8000, () => console.log(`server successfully runs : http://${HOST}:${PORT}`)); // http://localhost:8000
+
+const app = http.createServer((req, res) => {
+    console.log(req.method);
     res.end("<h1>wellcome to my first node.js server</h1>")
-
-}).listen(8000, () => console.log(`server successfully runs : http://${HOST}:${PORT}`)); // http://localhost:8000
+});
+app.listen(8000, () => console.log(`server successfully runs : http://${HOST}:${PORT}`));

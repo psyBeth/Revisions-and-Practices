54f4b08444a469151870792476501236ac6e7e8a
Betul <betulkoru06@gmail.com>
2024-02-28 21:31:56 +0300

    app.get

--- expressJS-routing/index.js
+++ expressJS-routing/index.js
@@ -18,7 +18,21 @@ const app = express();  // created a server on express
 // app.put('/', (req, res) => { res.send({"message" : "PUT method request"})})
 // app.delete('/', (req, res) => { res.send({"message" : "DELETE method request"})})
 
-app.all('/', app.post('/', (req, res) => { res.send({"message" : "ALL method called"})}))
+// app.all('/', app.post('/', (req, res) => { res.send({"message" : "ALL method called"})}))
+
+// joker characters
+// app.get('/',(req,res)=>{ res.send({ "message":"GET method called" })})
+// app.get('/ab(x?)12',(req,res)=>{ res.send({ "message":"GET method called" })}) //araya tek karakter ne olursa
+// app.get('/ab(x+)12',(req,res)=>{ res.send({ "message":"GET method called" })})
+
+// app.get('/ab*',(req,res)=>{ res.send({ "message":"GET method called" })}) // sonu ne olursa olsun
+// app.get('/a*b',(req,res)=>{ res.send({ "message":"GET method called" })}) // sonu ne olursa olsun
+
+// regex
+//! iki / arasında regex kabul edilir  ' kullanmayın
+// regexr.com
+app.get(/abc$/ ,(req,res)=>{ res.send({ "message":"GET method called" })}) // sonu ne olursa olsun
 
 
-app.listen(PORT, HOST, () => console.log(`server succeed on http://${HOST}:${PORT}`))
+
+app.listen(PORT,HOST,()=>console.log(`Server runned http://${HOST}:${PORT}`))

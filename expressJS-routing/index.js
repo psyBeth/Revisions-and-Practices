a3ea415cccbda49dc7b3e9553888a21a244721c5
Betul <betulkoru06@gmail.com>
2024-02-28 20:30:08 +0300

    created a simple express route

--- expressJS-routing/index.js
+++ expressJS-routing/index.js
@@ -2,10 +2,34 @@
 /*
     Expressjs Routing
 */
+// $ npm i 
+// or npm i express dotenv
+
 require('dotenv').config(); 
 
 
 const PORT=process.env?.PORT || 8000
 const HOST=process.env?.HOST || "127.0.0.1"
 
-app.listen(8000,()=>console.log(`server runned : http://${HOST}:${PORT}`))
+const express = require('express');
+const app = express();  // created a server on express
+
+app.get('/', (req, res) => {
+    // res.write("hello")
+    // res.end()
+
+    // res.write(JSON.stringify({
+    //     message : "hello"
+    // }))
+    // res.end()
+
+    // console.log("hello world!");
+    // res.send()
+
+    res.send({
+        "message" : "hello"
+    })
+//     res.send('hello') // no send more than once
+ })
+
+app.listen(PORT, HOST, () => console.log(`server succeed on http://${HOST}:${PORT}`))

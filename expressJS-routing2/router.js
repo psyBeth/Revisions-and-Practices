2ed406bec69b13d0ef2c4d033ba16785fa3d063f
Betul <betulkoru06@gmail.com>
2024-02-29 20:42:55 +0300

    router methods testing with thunder client

--- expressJS-routing2/router.js
+++ expressJS-routing2/router.js
@@ -15,7 +15,22 @@ const PORT = process.env.PORT || 8000;
 // express.Router()
 const router = express.Router()
 
-router.get('/', (req, res) => { res.send('Home Area') })
+// 3 URLs 3 methods
+// router.get('/', (req, res) => res.send({ message: "Home Area" }))
+// router.get('/about', (req, res) => res.send({ message: "About Area" }))
+// router.get('/user/:id', (req, res) => res.send({ message: req.params.id }))
+
+// 1 URL multiple methods
+//? router.route()
+router.route('/')
+    .get((req, res) => res.send({ message: "get" }))
+    .post((req, res) => res.send({ message: "post" }))
+    .put((req, res) => res.send({ message: "put" }))
+    .delete((req, res) => res.send({ message: "delete" })) 
+    //each one is a route
+
+//? After finished router-design, it will call with middleware:
+app.use(router)
 
 /* ------------------------------------------------------- */
 app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));

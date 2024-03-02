f45e2b9d9a8006750fa7e7c0cebe1a7aa4af7b18
Betul <betulkoru06@gmail.com>
2024-03-02 11:31:24 +0300

    getUserID error handle try-catch

--- expressJS-errorManagement/error.js
+++ expressJS-errorManagement/error.js
@@ -15,6 +15,30 @@ app.get('/', (req, res) => {
     throw new Error('error message')
 })
 
+app.get('/user/:id', (req, res) => {
+    const id = req.params?.id || 0
+
+    try{ 
+
+        if(isNaN(id)){
+            throw new Error('ID is not a number', { cause: 'params.id = ' + id })
+        } else {
+            res.send({
+                message: 'OK',
+                id
+            })
+        }
+
+    } catch(err) {
+
+        res.send({
+            error: true,
+            message: err.message,
+        })
+
+    }
+
+});
 
 /* ------------------------------------------------------- */
 app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
